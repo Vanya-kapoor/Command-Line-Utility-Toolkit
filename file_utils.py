2d7e57d3bf546b32ed83b1ae2e033981e51fd0dc
Vanya Kapoor <vanyakapoor1010@gmail.com>
2025-12-17 22:43:27 +0530

    final uploaded

--- file_utils.py
+++ file_utils.py
@@ -1,1 +1,5 @@
 import os
+def file_size(path):
+    if os.path.exists(path):
+        return os.path.getsize(path)
+    return -1
---
+++ main.py
@@ -0,0 +1,12 @@
+from constants import *
+from file_utils import *
+from math_utils import *
+from string_utils import *
+print(APP_NAME,Version)
+primenumber=int(input("enter prime number:"))
+print(isprime(primenumber))
+radius=int(input("enter radius:"))
+print(circlearea(radius))
+print(wordcount("hello python world"))
+print(reverseword("this is vanya kapoor"))
+print(file_size("test.txt"))
